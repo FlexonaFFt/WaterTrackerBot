b3cd48d11a6dac74765144286fe0f2eaee5ec003
FlexonaFFt <flexonafft@bk.ru>
2024-06-19 15:13:35 +0300

    --load something wrong solution

--- FirstDjangoProj/config/myapp/views.py
+++ FirstDjangoProj/config/myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from .models import Author, Book, Genre
+from serializers import BookSerializer, BookCreateSerializer
 
 def authors(request):
     authors = Author.objects.all()
@@ -21,3 +23,14 @@ def genres(request):
         'genres': genres
     }
     return render(request, 'genres.html', context)
+
+def add_book(request):
+    return render(request, 'add_book.html')
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update']:
+            return BookCreateSerializer
+        return BookSerializer

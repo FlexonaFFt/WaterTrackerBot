5bc4e6b1b127351b46ca0d7b0f58f48cd4f76fec
FlexonaFFt <flexonafft@bk.ru>
2024-06-22 20:57:27 +0300

    --load version3 --unstable 2

--- version3/app/config/myapp/admin.py
+++ version3/app/config/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class TelegramUser(admin.ModelAdmin):
+
+    list_display = ('phone_number', 'adress', 'is_active')
